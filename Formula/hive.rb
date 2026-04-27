72ea203187265568803597d94fdb02841b397588
Robbie Diaz <metaldrummer610@gmail.com>
2026-04-27 15:21:29 -0400

    fix: point formula urls at hive-releases public repo

--- Formula/hive.rb
+++ Formula/hive.rb
@@ -3,11 +3,11 @@ class Hive < Formula
   homepage "https://hivemine-technologies.github.io/hive/"
   version "0.1.0"
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Hivemine-Technologies/hive/releases/download/v0.1.0/hive-aarch64-apple-darwin.tar.xz"
+    url "https://github.com/Hivemine-Technologies/hive-releases/releases/download/v0.1.0/hive-aarch64-apple-darwin.tar.xz"
     sha256 "d2e4c9959159cf501c34f738f66174ae17b0de28af88b4a0c49d3e0af77cb36d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Hivemine-Technologies/hive/releases/download/v0.1.0/hive-x86_64-unknown-linux-gnu.tar.xz"
+    url "https://github.com/Hivemine-Technologies/hive-releases/releases/download/v0.1.0/hive-x86_64-unknown-linux-gnu.tar.xz"
     sha256 "1d5d8237df629c8af2652f3c7ea38c78564be8164587fc6d4d2f509ddaffb361"
   end
   license "LicenseRef-Proprietary"
